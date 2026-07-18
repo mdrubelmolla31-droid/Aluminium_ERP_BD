cf6d355c3556ddcf43945b325c8bcf2cd9ac9a73
mdrubelmolla31-droid <mdrubelmolla31@gmail.com>
2026-07-18 13:15:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function login() {
     }
 
 }
+function calculateMaterial(){
+
+let w=parseFloat(document.getElementById("width").value);
+let h=parseFloat(document.getElementById("height").value);
+let q=parseInt(document.getElementById("qty").value);
+
+if(!w || !h || !q){
+
+alert("সব তথ্য দিন");
+
+return;
+
+}
+
+let glass=((w*h)/144)*q;
+
+document.getElementById("result").innerHTML=`
+
+<h2>Result</h2>
+
+<p>Glass : ${glass.toFixed(2)} Sqft</p>
+
+<p>Quantity : ${q}</p>
+
+<p><b>Formula Engine Coming...</b></p>
+
+`;
+
+}

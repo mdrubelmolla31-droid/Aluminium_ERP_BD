34eaa8ba0efee59b1a71dad8b130c7f71da64c20
mdrubelmolla31-droid <mdrubelmolla31@gmail.com>
2026-07-20 16:05:25 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,32 +1,55 @@
-function saveSettings(){
+function saveSettings() {
 
-let data={
+let settings = {
 
-aluRate:parseFloat(document.getElementById("aluRate").value)||0,
+company: document.getElementById("company").value,
 
-glassRate:parseFloat(document.getElementById("glassRate").value)||0,
+series: document.getElementById("series").value,
 
-labourRate:parseFloat(document.getElementById("labourRate").value)||0,
+aluRate: parseFloat(document.getElementById("aluRate").value) || 0,
 
-profit:parseFloat(document.getElementById("profit").value)||0
+glassCompany: document.getElementById("glassCompany").value,
+
+glassThickness: document.getElementById("glassThickness").value,
+
+glassColour: document.getElementById("glassColour").value,
+
+glassRate: parseFloat(document.getElementById("glassRate").value) || 0,
+
+labourRate: parseFloat(document.getElementById("labourRate").value) || 0,
+
+profit: parseFloat(document.getElementById("profit").value) || 0
 
 };
 
-localStorage.setItem("erpSettings",JSON.stringify(data));
+localStorage.setItem("erpSettings", JSON.stringify(settings));
 
-alert("Settings Saved");
-
-}
-
-window.onload=function(){
-
-let s=JSON.parse(localStorage.getItem("erpSettings"));
-
-if(!s)return;
-
-document.getElementById("aluRate").value=s.aluRate;
-document.getElementById("glassRate").value=s.glassRate;
-document.getElementById("labourRate").value=s.labourRate;
-document.getElementById("profit").value=s.profit;
+alert("Settings Saved Successfully");
 
 }
+
+window.onload = function () {
+
+let settings = JSON.parse(localStorage.getItem("erpSettings"));
+
+if (!settings) return;
+
+document.getElementById("company").value = settings.company;
+
+document.getElementById("series").value = settings.series;
+
+document.getElementById("aluRate").value = settings.aluRate;
+
+document.getElementById("glassCompany").value = settings.glassCompany;
+
+document.getElementById("glassThickness").value = settings.glassThickness;
+
+document.getElementById("glassColour").value = settings.glassColour;
+
+document.getElementById("glassRate").value = settings.glassRate;
+
+document.getElementById("labourRate").value = settings.labourRate;
+
+document.getElementById("profit").value = settings.profit;
+
+};

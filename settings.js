8fb902477e3ec54321c570c3e2aac79ee4bc0f3b
mdrubelmolla31-droid <mdrubelmolla31@gmail.com>
2026-07-20 16:25:08 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,26 +1,20 @@
 let rates = JSON.parse(localStorage.getItem("rates")) || [];
 
-function saveSettings() {
+function saveSettings(){
 
 let data = {
 
 company: document.getElementById("company").value,
-
 series: document.getElementById("series").value,
 
 glassCompany: document.getElementById("glassCompany").value,
-
 glassThickness: document.getElementById("glassThickness").value,
-
 glassColour: document.getElementById("glassColour").value,
 
-aluRate: parseFloat(document.getElementById("aluRate").value),
-
-glassRate: parseFloat(document.getElementById("glassRate").value),
-
-labourRate: parseFloat(document.getElementById("labourRate").value),
-
-profit: parseFloat(document.getElementById("profit").value)
+aluRate: Number(document.getElementById("aluRate").value),
+glassRate: Number(document.getElementById("glassRate").value),
+labourRate: Number(document.getElementById("labourRate").value),
+profit: Number(document.getElementById("profit").value)
 
 };
 
@@ -28,48 +22,35 @@ rates.push(data);
 
 localStorage.setItem("rates", JSON.stringify(rates));
 
-showRates();
-
-alert("Rate Saved");
+loadRates();
 
 }
 
-function showRates() {
+function loadRates(){
 
 let body = document.getElementById("rateBody");
 
 body.innerHTML = "";
 
-rates.forEach((r, i) => {
+rates.forEach((r,index)=>{
 
 body.innerHTML += `
-
 <tr>
 
 <td>${r.company}</td>
-
 <td>${r.series}</td>
-
 <td>${r.glassCompany}</td>
-
 <td>${r.glassThickness}</td>
-
 <td>${r.aluRate}</td>
-
 <td>${r.glassRate}</td>
-
 <td>${r.labourRate}</td>
-
 <td>${r.profit}%</td>
 
 <td>
-
-<button onclick="deleteRate(${i})">Delete</button>
-
+<button onclick="deleteRate(${index})">Delete</button>
 </td>
 
 </tr>
-
 `;
 
 });
@@ -80,10 +61,10 @@ function deleteRate(index){
 
 rates.splice(index,1);
 
-localStorage.setItem("rates",JSON.stringify(rates));
+localStorage.setItem("rates", JSON.stringify(rates));
 
-showRates();
+loadRates();
 
 }
 
-window.onload = showRates;
+window.onload = loadRates;

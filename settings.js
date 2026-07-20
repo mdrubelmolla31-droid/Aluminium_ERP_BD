b79cdc2b9f394a40daae40385a299f606b21970f
mdrubelmolla31-droid <mdrubelmolla31@gmail.com>
2026-07-20 20:40:03 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,12 @@
 let rates = JSON.parse(localStorage.getItem("rates")) || [];
 
 function saveSettings(){
-
-let rate={
+let rate = {
 
 company:document.getElementById("company").value,
 series:document.getElementById("series").value,
+
+aluThickness:document.getElementById("aluThickness").value,
 
 glassCompany:document.getElementById("glassCompany").value,
 glassThickness:document.getElementById("glassThickness").value,
@@ -13,7 +14,12 @@ glassColour:document.getElementById("glassColour").value,
 
 aluRate:Number(document.getElementById("aluRate").value),
 glassRate:Number(document.getElementById("glassRate").value),
+
+hardwareRate:Number(document.getElementById("hardwareRate").value),
+fittingsRate:Number(document.getElementById("fittingsRate").value),
+
 labourRate:Number(document.getElementById("labourRate").value),
+
 profit:Number(document.getElementById("profit").value)
 
 };

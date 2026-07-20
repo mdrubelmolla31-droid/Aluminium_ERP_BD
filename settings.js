61398eadf1e4ed542b53e8beffb7919c0c1938e8
mdrubelmolla31-droid <mdrubelmolla31@gmail.com>
2026-07-20 16:37:44 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,25 +2,47 @@ let rates = JSON.parse(localStorage.getItem("rates")) || [];
 
 function saveSettings(){
 
-let data = {
+let rate={
 
-company: document.getElementById("company").value,
-series: document.getElementById("series").value,
+company:document.getElementById("company").value,
+series:document.getElementById("series").value,
 
-glassCompany: document.getElementById("glassCompany").value,
-glassThickness: document.getElementById("glassThickness").value,
-glassColour: document.getElementById("glassColour").value,
+glassCompany:document.getElementById("glassCompany").value,
+glassThickness:document.getElementById("glassThickness").value,
+glassColour:document.getElementById("glassColour").value,
 
-aluRate: Number(document.getElementById("aluRate").value),
-glassRate: Number(document.getElementById("glassRate").value),
-labourRate: Number(document.getElementById("labourRate").value),
-profit: Number(document.getElementById("profit").value)
+aluRate:Number(document.getElementById("aluRate").value),
+glassRate:Number(document.getElementById("glassRate").value),
+labourRate:Number(document.getElementById("labourRate").value),
+profit:Number(document.getElementById("profit").value)
 
 };
 
-rates.push(data);
+// একই Combination থাকলে Update করবে
 
-localStorage.setItem("rates", JSON.stringify(rates));
+let index=rates.findIndex(r=>
+
+r.company==rate.company &&
+r.series==rate.series &&
+r.glassCompany==rate.glassCompany &&
+r.glassThickness==rate.glassThickness &&
+r.glassColour==rate.glassColour
+
+);
+
+if(index>=0){
+
+rates[index]=rate;
+
+}else{
+
+rates.push(rate);
+
+}
+
+localStorage.setItem("rates",JSON.stringify(rates));
+
+alert("Rate Saved Successfully");
 
 loadRates();
 
@@ -28,43 +50,52 @@ loadRates();
 
 function loadRates(){
 
-let body = document.getElementById("rateBody");
+let body=document.getElementById("rateBody");
 
-body.innerHTML = "";
+body.innerHTML="";
 
-rates.forEach((r,index)=>{
+rates=JSON.parse(localStorage.getItem("rates"))||[];
 
-body.innerHTML += `
+rates.forEach((r,i)=>{
+
+body.innerHTML+=`
+
 <tr>
 
 <td>${r.company}</td>
 <td>${r.series}</td>
 <td>${r.glassCompany}</td>
 <td>${r.glassThickness}</td>
+<td>${r.glassColour}</td>
 <td>${r.aluRate}</td>
 <td>${r.glassRate}</td>
 <td>${r.labourRate}</td>
 <td>${r.profit}%</td>
 
 <td>
-<button onclick="deleteRate(${index})">Delete</button>
+
+<button onclick="deleteRate(${i})">
+Delete
+</button>
+
 </td>
 
 </tr>
+
 `;
 
 });
 
 }
 
-function deleteRate(index){
+function deleteRate(i){
 
-rates.splice(index,1);
+rates.splice(i,1);
 
-localStorage.setItem("rates", JSON.stringify(rates));
+localStorage.setItem("rates",JSON.stringify(rates));
 
 loadRates();
 
 }
 
-window.onload = loadRates;
+window.onload=loadRates;

205991833c45ddfbf61697bf7e0fe56e1f8cd73e
mdrubelmolla31-droid <mdrubelmolla31@gmail.com>
2026-09-26 01:59:11 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'erp-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // ক্যাশ থেকে লোড করবে
+        }
+        return fetch(event.request); // নেটওয়ার্ক থেকে লোড করবে
+      })
+  );
+});
